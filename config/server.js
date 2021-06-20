3a734c5262e6a08783dad9772864ccb2439484df
KSenior2608 <ashishkumaras789@gmail.com>
2021-06-21 00:34:42 +0530

    Fixing deployment bug

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int(process.env.PORT, 1337),
+  port: env.int(PORT, 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'aefdda1be340bb94251b0b98b386a811'),
